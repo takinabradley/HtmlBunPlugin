87568f2fa9a946e81b84827306874af9d8aa8917
Bradley Finney <77649883+takinabradley@users.noreply.github.com>
2023-09-17 07:45:49 -0400

    Build new version

--- out/HtmlBunPlugin.js
+++ out/HtmlBunPlugin.js
@@ -1,22 +1,50 @@
 // @bun
-// HtmlBunPlugin.ts
+// src/default.html
+var default_default = "./default-66fbc1e590c0cb2c.html";
+
+// src/HtmlBunPlugin.ts
 import fs from "fs/promises";
-var createDefaultHTML = function(entrypoints) {
+
+// src/createHtmlCloneWithScriptTags.ts
+var addScriptTags = function(el, entrypoints, insertComments = true) {
   const fileNames = filePathsToFileNames(entrypoints);
-  const scriptTags = fileNames.map((fileName) => `\n  <script src='./${fileName}' type='module'></script>`).join("");
-  const defaultHTML = `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>${scriptTags}
-</head>
-<body>
-  
-</body>
-</html>`;
-  return new File([defaultHTML], "index.html", { type: "text/html" });
+  const scriptTags = fileNames.map((fileName) => `  <script src='./${fileName}' type='module'></script>\n`);
+  if (insertComments)
+    el.append("  <!-- The following <script> tags added via HTMLBunPlugin -->\n", { html: true });
+  scriptTags.forEach((script) => el.append(script, { html: true }));
+  if (insertComments)
+    el.append("  <!-- End of HtmlBunPlugin script tags -->\n", { html: true });
 };
+var addTitleTag = function(el, title, insertComments = true) {
+  if (insertComments)
+    el.append("  <!-- The following <title> added via HtmlBunPlugin -->\n", { html: true });
+  el.append(`  <title>${title}</title>\n`, { html: true });
+};
+var filePathsToFileNames = function(filePaths) {
+  return filePaths.map((entry) => {
+    const lastIndexOfSlash = entry.lastIndexOf("/");
+    return lastIndexOfSlash >= 0 ? entry.slice(entry.lastIndexOf("/") + 1) : entry;
+  });
+};
+var rewriter = new HTMLRewriter;
+async function createHtmlCloneWithScriptTags(htmlFilePath, entrypoints, fileName, title) {
+  const headElementHandler = {
+    element(el) {
+      if (el.tagName !== "head")
+        return;
+      addScriptTags(el, entrypoints);
+      if (title)
+        addTitleTag(el, title);
+    }
+  };
+  const bunFileUrl = Bun.pathToFileURL(htmlFilePath).href;
+  const originalHtmlRes = await fetch(bunFileUrl);
+  const newHtmlRes = rewriter.on("head", headElementHandler).transform(originalHtmlRes);
+  const newHtmlBlob = await newHtmlRes.blob();
+  return new File([newHtmlBlob], fileName, { type: newHtmlBlob.type });
+}
+
+// src/HtmlBunPlugin.ts
 var HtmlBunPlugin = function(config = { filename: "index.html", title: "Bun App" }) {
   return {
     name: "HtmlBunPlugin",
@@ -24,17 +52,16 @@ var HtmlBunPlugin = function(config = { filename: "index.html", title: "Bun App"
       if (!build.config.outdir)
         return;
       const outdir = build.config.outdir;
-      const file = createDefaultHTML(build.config.entrypoints);
+      const file = await createHtmlCloneWithScriptTags(config.template || default_default, build.config.entrypoints, config.filename, config.title);
       try {
         await fs.writeFile(`${outdir}/${config.filename}`, await file.arrayBuffer());
       } catch {
-        await fs.mkdir(outdir);
+        await fs.mkdir(outdir, { recursive: true });
         await fs.writeFile(`${outdir}/${config.filename}`, await file.arrayBuffer());
       }
     }
   };
 };
-var filePathsToFileNames = (entrypoints) => entrypoints.map((entry) => entry.slice(entry.lastIndexOf("/") + 1));
 var HtmlBunPlugin_default = HtmlBunPlugin;
 export {
   HtmlBunPlugin_default as default
