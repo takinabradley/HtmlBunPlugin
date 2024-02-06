4ff6e72234bfe9a14b26dfbff49349f756c29c32
Johan van Eck <johveck@gmail.com>
2024-02-06 20:39:45 +0200

    Update .d.ts file

--- out/HtmlBunPlugin.d.ts
+++ out/HtmlBunPlugin.d.ts
@@ -1,9 +1,10 @@
-import type { BunPlugin } from 'bun';
+import type { BunPlugin } from "bun";
+
 export interface HtmlBunPluginConfig {
-    // something
-    filename: string
-    title?: string;
-    template?: string;
+  filename: string;
+  title?: string;
+  template?: string;
+  publicPath?: string;
 }
 /**
  *
@@ -11,6 +12,8 @@ export interface HtmlBunPluginConfig {
  * - filename: Defaults to 'index.html'. Required if passing in an options object.
  * - title?: adds a title tag with the provided string. Defaults to 'Bun App'
  * - template?: path to a template HTML file to inject entries and title into
+ * - publicPath?: the base path that the entrypoint will be fetched from, defaults to './'
  */
 declare function HtmlBunPlugin(config?: HtmlBunPluginConfig): BunPlugin;
+
 export default HtmlBunPlugin;
